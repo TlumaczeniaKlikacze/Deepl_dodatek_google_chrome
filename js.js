1ff555e5a74b3d9e1f1cc9fa0933361fe2c71dd1
TlumaczeniaKlikacze <89144762+TlumaczeniaKlikacze@users.noreply.github.com>
2021-10-13 12:20:58 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -131,96 +131,103 @@ const good_tags = ['p','strong','ul','li','table','tbody','tr','th','thead','td'
 const interval = setInterval(() => {
     if(document.querySelector("#tableRowTextEditTabs_0")){
         clearInterval(interval)
-        const container = document.querySelector("#tableRowTextEditTabs_0").childNodes[1]
-const el = document.createElement('li')
-el.innerHTML = "Deepl_click_1"
-el.className = '_tableRowTextEditTabs_tab_wysiwig';
-el.style.color ="green"
-el.id="Deepl_for_free_translate"
-container.appendChild(el)
-const el2 = document.createElement('li')
-el2.innerHTML = "Deepl_click_2"
-el2.className = '_tableRowTextEditTabs_tab_wysiwig';
-el2.style.color = 'blue'
-el2.id = "Deepl_reverse_tag";
-container.appendChild(el2)
-document.querySelector("#Deepl_reverse_tag").addEventListener("click",()=>{
-    console.log('xddd')
-    let text_html = document.querySelector("#tableRowTextEditTabs_container_html_area_0")
-    let text = text_html.value;
-    for(let _=0;_<db.length;_++){
-        //zamieniam tagi na inne tagi lol xd
-        if(text.includes(db[_].for_deepl)){
-            //jesli zawiera w sobie
-            text = text.replace(new RegExp(`${db[_].for_deepl}`,'g'),`\n${db[_].original}\n`)
+        for(let _=0;_<30;_++){
+            if(document.querySelector(`a[href="#divIdshopLangDesc_${_}"]`)!=null){
+                    work(_)
+            }
         }
     }
-    text_html.value = text;
-    document.querySelector("#tableRowTextEditTabs_tab_preview_0").childNodes[0].click()
-})
 
-document.querySelector("#Deepl_for_free_translate").addEventListener('click',()=>{
 
-    const button_edit = document.querySelector("#tableRowTextEditTabs_tab_html_0").childNodes[0].click()//pobieram przycisk
-    let text_html = document.querySelector("#tableRowTextEditTabs_container_html_area_0")
-    let text= text_html.value
-    text_html.value = ''
-    let ready_text = ``
-    let tmp;
-    let tmp2
-    let splitet_text = text.split('<')
-    for(let _=0;_<splitet_text.length;_++){
-        tmp = splitet_text[_].split(">")
-        tmp2= tmp[0].split(' ')
-        if(tmp[1] != undefined){
-            for(let __ =0;__<tags.length;__++){
-                if(tmp2[0] == tags[__] || tmp2[0] == `/${tags[__]}`){
-                    if(tmp[1].trim() != undefined){
-                        if(tmp2[0] == "h1" || tmp2[0] == "h2"|| tmp2[0] == "h3" || tmp2[0] == "h4" || tmp2[0] == "h5"|| tmp2[0] == "h6"){
-                            console.log(tmp2[0])
-                            ready_text +=`<strong>${tmp[1]}`
-                        }else if(tmp2[0] == "/h1" || tmp2[0] == "/h2"|| tmp2[0] == "/h3" || tmp2[0] == "/h4" || tmp2[0] == "/h5"|| tmp2[0] == "/h6"){
-                            ready_text +=`</strong>${tmp[1]}`
-                        }else{
-                            ready_text +=tmp[1]
+}, 200);
+
+const work = (num)=>{
+    const container = document.querySelector(`#tableRowTextEditTabs_${num}`).childNodes[1]
+    const el = document.createElement('li')
+    el.innerHTML = "Deepl_click_1"
+    el.className = '_tableRowTextEditTabs_tab_wysiwig';
+    el.style.color ="green"
+    el.id=`Deepl_for_free_translate_${num}`
+    container.appendChild(el)
+    const el2 = document.createElement('li')
+    el2.innerHTML = "Deepl_click_2"
+    el2.className = '_tableRowTextEditTabs_tab_wysiwig';
+    el2.style.color = 'blue'
+    el2.id = `Deepl_reverse_tag_${num}`;
+    container.appendChild(el2)
+    document.querySelector(`#Deepl_reverse_tag_${num}`).addEventListener("click",()=>{
+        let text_html = document.querySelector(`#tableRowTextEditTabs_container_html_area_${num}`)
+        let text = text_html.value;
+        for(let _=0;_<db.length;_++){
+            //zamieniam tagi na inne tagi lol xd
+            if(text.includes(db[_].for_deepl)){
+                //jesli zawiera w sobie
+                text = text.replace(new RegExp(`${db[_].for_deepl}`,'g'),`\n${db[_].original}\n`)
+            }
+        }
+        text_html.value = text;
+        document.querySelector(`#tableRowTextEditTabs_tab_preview_${num}`).childNodes[0].click()
+    })
+    
+    document.querySelector(`#Deepl_for_free_translate_${num}`).addEventListener('click',()=>{
+    
+        const button_edit = document.querySelector(`#tableRowTextEditTabs_tab_html_${num}`).childNodes[0].click()//pobieram przycisk
+        let text_html = document.querySelector(`#tableRowTextEditTabs_container_html_area_${num}`)
+        let text= text_html.value
+        text_html.value = ''
+        let ready_text = ``
+        let tmp;
+        let tmp2
+        let splitet_text = text.split('<')
+        ready_text += splitet_text[0]
+        for(let _=0;_<splitet_text.length;_++){
+            if(_==splitet_text.length-1){
+                ready_text +=`<${splitet_text[_]}` 
+            }else if(_!=splitet_text.length-1){
+                tmp = splitet_text[_].split(">")
+                tmp2= tmp[0].split(' ')
+                if(tmp[1] != undefined){
+                    for(let __ =0;__<tags.length;__++){
+                        if(tmp2[0] == tags[__] || tmp2[0] == `/${tags[__]}`){
+                            if(tmp[1].trim() != undefined){
+                                if(tmp2[0] == "h1" || tmp2[0] == "h2"|| tmp2[0] == "h3" || tmp2[0] == "h4" || tmp2[0] == "h5"|| tmp2[0] == "h6"){
+                                    ready_text +=`<strong>${tmp[1].trim()}`
+                                }else if(tmp2[0] == "/h1" || tmp2[0] == "/h2"|| tmp2[0] == "/h3" || tmp2[0] == "/h4" || tmp2[0] == "/h5"|| tmp2[0] == "/h6"){
+                                    ready_text +=`</strong>${tmp[1].trim()}`
+                                }else{
+                                    ready_text +=tmp[1].trim()
+                                }
+                            }
+                            break;
                         }
                     }
-                    break;
+                    for(let ___=0;___<good_tags.length;___++){
+                        if(tmp2[0] == good_tags[___] || tmp2[0] == `/${good_tags[___]}`){
+                           if(tmp2[0][0] == '/'){
+                            ready_text += `${tmp[1]}<${tmp2[0].trim()}>`
+                           }else{
+                            ready_text += `<${tmp2[0]}>${tmp[1].trim()}`
+                           }
+                            break;
+                        }
+                    }
                 }
             }
-            for(let ___=0;___<good_tags.length;___++){
-                if(tmp2[0] == good_tags[___] || tmp2[0] == `/${good_tags[___]}`){
-                   if(tmp2[0][0] == '/'){
-                    ready_text += `${tmp[1]}<${tmp2[0]}>`
-                   }else{
-                    ready_text += `<${tmp2[0]}>${tmp[1]}`
-                   }
-                    break;
-                }
-            }
+           
+        }
+
+    
+    for(let _=0;_<db.length;_++){
+        //zamieniam tagi na inne tagi lol xd
+        if(ready_text.includes(db[_].original)){
+            //jesli zawiera w sobie
+            ready_text = ready_text.replace(new RegExp(`${db[_].original}`,'g'),`\n${db[_].for_deepl}\n`)
         }
     }
-    let p = new RegExp(/<p>\s{1,}<[/]p>/g)
-    let strong =  new RegExp(/<strong>\s{1,}<[/]strong>/g)
-  
-
-    ready_text = ready_text.replace(p,"")
-    ready_text = ready_text.replace(strong,"")
-    
-
-// let reg = new RegExp(/(?<=\<).+?(?=\>)/g)
-for(let _=0;_<db.length;_++){
-    //zamieniam tagi na inne tagi lol xd
-    if(ready_text.includes(db[_].original)){
-        //jesli zawiera w sobie
-        ready_text = ready_text.replace(new RegExp(`${db[_].original}`,'g'),`\n${db[_].for_deepl}\n`)
-    }
+    text_html.value = ready_text;
+    text_html.select();
+    document.execCommand("copy")
+    text_html.value = ``;
+    })
 }
-text_html.value = ready_text;
-text_html.select();
-document.execCommand("copy")
-text_html.value = ``;
-})
-    }
-}, 200);
 
